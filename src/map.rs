6912ef7bae2efaeb79539e9ce763943ab0f808e2
awski <adam.wolanski@protonmail.com>
2021-04-08 18:00:44 +0200

    map module rework

--- src/map.rs
+++ src/map.rs
@@ -1,5 +1,24 @@
 //TODO(#1): rework map impl
 
+pub const MAP_WIDTH: i32 = 80;
+pub const MAP_HEIGHT: i32 = 45;
+
+pub type Map = Vec<Vec<Tile>>;
+
+#[derive(Clone)]
+pub struct Tile {
+    pub blocked: bool,
+    pub blocked_sight: bool,
+}
+impl Tile {
+    pub fn empty() -> Self {
+        Tile { blocked: false, blocked_sight: false }
+    }
+    pub fn wall() -> Self {
+        Tile { blocked: true, blocked_sight: true }
+    }
+}
+
 struct Rect {
     x1: i32,
     y1: i32,
@@ -17,12 +36,32 @@ impl Rect {
     }
 }
 
-pub fn create_map() {
-    unimplemented!();
+pub fn xx_create_map() -> Map {
+    let mut map = vec![vec![Tile::empty(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
+
+    map[40][20] = Tile::wall();
+    map[30][20] = Tile::wall();
+
+    map
 }
 
-pub fn create_room() {
-    unimplemented!();
+pub fn create_map() -> Map {
+    let mut map = vec![vec![Tile::empty(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
+
+    let room1 = Rect::new(20, 15, 10, 10);
+    let room2 = Rect::new(50, 15, 10, 10);
+    create_room(room1, &mut map);
+    create_room(room2, &mut map);
+
+    map
+}
+
+fn create_room(room: Rect, map: &mut Map) {
+    for x in (room.x1 + 1)..room.x2 {
+        for y in (room.y1 + 1)..room.y2 {
+            map[x as usize][y as usize] = Tile::empty();
+        }
+    }
 }
 
 pub fn render_map() {
